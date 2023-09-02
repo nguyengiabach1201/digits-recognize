8509a1ec825b63a571d6ef880260640300263332
nguyengiabach1201 <108968549+nguyengiabach1201@users.noreply.github.com>
2023-09-02 11:11:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ def predict(model, img_path, class_names):
     img_array = tf.keras.preprocessing.image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)
 
-    print(img_array)
-
     predictions = model.predict(img_array)
     predicted_class = class_names[np.argmax(predictions[0])]
     confidence = round(100 * np.max(predictions[0]), 2)
